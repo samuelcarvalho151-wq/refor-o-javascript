2e1db76c7fb231b03f0e70eba9401bb5d8500334
SAMUEL DOS SANTOS CARVALHO <samuel_carvalho151@estudante.sesisenai.org.br>
2026-08-18 15:33:44 -0300

    js1

--- exercicios.js
+++ exercicios.js
@@ -19,8 +19,14 @@
 // Crie 2 variáveis com números e mostre no console
 // o resultado da soma, subtração, multiplicação e divisão.
 // ------------------------------------------------------------
+  
+let num1 = 10;
+let num2 = 5;
 
-
+console.log("Soma:", num1 + num2);
+console.log("Subtração:", num1 - num2);
+console.log("Multiplicação:", num1 * num2);
+console.log("Divisão:", num1 / num2);
 
 
 // EXERCÍCIO 2 - Conversor de temperatura
